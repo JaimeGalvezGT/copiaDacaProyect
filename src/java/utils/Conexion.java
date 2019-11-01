31c744b1c21f59ed88b6cac8aeb628d0bee76d8d
OscarMartinezTambito <43862039+OscarMartinezTambito@users.noreply.github.com>
2019-11-01 10:02:14 -0600

    beta

--- src/java/utils/Conexion.java
+++ src/java/utils/Conexion.java
@@ -20,7 +20,7 @@ import java.sql.SQLException;
 public class Conexion implements Serializable{
      //Atributos de la conexion
     private String host = "localhost";
-    private String port = "3306";
+    private String port = "3307";
     private String userName = "root";
     private String password = "admon";
     private String dbName = "daca";
